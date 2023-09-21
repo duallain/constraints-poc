f38fd79fcdcdee5579c90a1d5cfe00af263ac0c0
Alan <duallain@gmail.com>
2023-09-20 19:21:10 -0700

    Adding a couple of new pr actions (#4)
    * Adding a couple of new pr actions
    to test new formatters
    
    * let's run the correct second linter
    
    * formatting, after confirming action failed

--- practical_project_examples/amphibian_coexistence.py
+++ practical_project_examples/amphibian_coexistence.py
@@ -1,39 +1,39 @@
 from ortools.linear_solver import pywraplp
 
+
 def solve_coexistence():
-    title = 'Amphibian coexistence'
-    solver = pywraplp.Solver(title,pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
-    
+    title = "Amphibian coexistence"
+    solver = pywraplp.Solver(title, pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+
     # 'answer' variables, represents a single group of amphibians
-    species = [solver.NumVar(0, 1000,'species[%i]' % i) for i in range(3)]
-    
+    species = [solver.NumVar(0, 1000, "species[%i]" % i) for i in range(3)]
+
     # total population, one of the constraints defines it
-    pop = solver.NumVar(0,3000,'pop')
-    
+    pop = solver.NumVar(0, 3000, "pop")
+
     # constraints
-    solver.Add(2*species[0] + species[1] + species[2] <= 1500)
-    solver.Add(species[0] + 3*species[1] + 2*species[2] <= 3000)
-    solver.Add(species[0] + 2*species[1] + 3*species[2] <= 4000)
-    
+    solver.Add(2 * species[0] + species[1] + species[2] <= 1500)
+    solver.Add(species[0] + 3 * species[1] + 2 * species[2] <= 3000)
+    solver.Add(species[0] + 2 * species[1] + 3 * species[2] <= 4000)
+
     solver.Add(pop == species[0] + species[1] + species[2])
-    
+
     solver.Maximize(pop)
-    
+
     # book doesn't have this line
     solver.Solve()
-    
-    
-    
-    return pop.SolutionValue(),[e.SolutionValue() for e in species]
 
-if __name__ == '__main__':
-    pop,x=solve_coexistence()
-    
-    Table=[['Specie', 'Count']]
-    
+    return pop.SolutionValue(), [e.SolutionValue() for e in species]
+
+
+if __name__ == "__main__":
+    pop, x = solve_coexistence()
+
+    Table = [["Specie", "Count"]]
+
     for i in range(3):
-        Table.append([['Toads','Salamanders','Caecilians'][i], x[i]])
-    
-    Table.append(['Total', pop])
-    for key,value in Table:
-        print(key,value)
+        Table.append([["Toads", "Salamanders", "Caecilians"][i], x[i]])
+
+    Table.append(["Total", pop])
+    for key, value in Table:
+        print(key, value)
