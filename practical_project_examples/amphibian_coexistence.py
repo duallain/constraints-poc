da74cac6d60718c3c7e801a51cbe6ef4c679e6d0
Alan Alexander <duallain@gmail.com>
2023-09-20 18:22:21 -0700

    Adding a couple of new pr actions to test new formatters

--- practical_project_examples/amphibian_coexistence.py
+++ practical_project_examples/amphibian_coexistence.py
@@ -1,36 +1,36 @@
 from ortools.linear_solver import pywraplp
 
+
 def solve_coexistence():
-    title = 'Amphibian coexistence'
-    solver = pywraplp.Solver(title,pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
-    
-    x = [solver.NumVar(0, 1000,'x[%i]' % i) for i in range(3)]
-        
-    pop = solver.NumVar(0,3000,'pop')
-    
-    solver.Add(2*x[0] + x[1] + x[2] <= 1500)
-    solver.Add(x[0] + 3*x[1] + 2*x[2] <= 3000)
-    solver.Add(x[0] + 2*x[1] + 3*x[2] <= 4000)
-    
+    title = "Amphibian coexistence"
+    solver = pywraplp.Solver(title, pywraplp.Solver.GLOP_LINEAR_PROGRAMMING)
+
+    x = [solver.NumVar(0, 1000, "x[%i]" % i) for i in range(3)]
+
+    pop = solver.NumVar(0, 3000, "pop")
+
+    solver.Add(2 * x[0] + x[1] + x[2] <= 1500)
+    solver.Add(x[0] + 3 * x[1] + 2 * x[2] <= 3000)
+    solver.Add(x[0] + 2 * x[1] + 3 * x[2] <= 4000)
+
     solver.Add(pop == x[0] + x[1] + x[2])
-    
+
     solver.Maximize(pop)
-    
+
     # book doesn't have this line
     solver.Solve()
-    
-    
-    
-    return pop.SolutionValue(),[e.SolutionValue() for e in x]
 
-if __name__ == '__main__':
-    pop,x=solve_coexistence()
-    
-    Table=[['Specie', 'Count']]
-    
+    return pop.SolutionValue(), [e.SolutionValue() for e in x]
+
+
+if __name__ == "__main__":
+    pop, x = solve_coexistence()
+
+    Table = [["Specie", "Count"]]
+
     for i in range(3):
-        Table.append([['Toads','Salamanders','Caecilians'][i], x[i]])
-    
-    Table.append(['Total', pop])
-    for key,value in Table:
-        print(key,value)
+        Table.append([["Toads", "Salamanders", "Caecilians"][i], x[i]])
+
+    Table.append(["Total", pop])
+    for key, value in Table:
+        print(key, value)
